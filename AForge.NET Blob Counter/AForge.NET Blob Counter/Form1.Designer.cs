cd01ffb515879e9c5e38dcef23101b92968334e9
Yasin <ysnlms@gmail.com>
2019-07-01 18:12:49 +0300

    Added

---
+++ AForge.NET Blob Counter/AForge.NET Blob Counter/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace AForge.NET_Blob_Counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(627, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(13, 519);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Analiz Et";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(519, 519);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 56);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "İleri";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(526, 578);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 578);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(140, 519);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 56);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Kenarları Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(267, 519);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 56);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Renklendir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(392, 519);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 56);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Küçükleri Gizle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(653, 615);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Şekil Sayma";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ AForge.NET Blob Counter/AForge.NET Blob Counter/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+
+namespace AForge.NET_Blob_Counter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+        Blob[] blobs;
+        int sayac;
+        int toplamSekilSayisi;
+        Bitmap kaynakResim;
+        Bitmap siyahBeyazResim;
+
+        //ilk önce bu butona tıklanmalı
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayac = 1;
+            kaynakResim = (Bitmap)System.Drawing.Image.FromFile("sample2.jpg");
+
+            //Otsu Threshold uygulandı
+            OtsuThreshold otsuFiltre = new OtsuThreshold();
+            //resim eğer renkliyse önce griye çeviriyor sonra filtre uyguluyor
+            //resim zaten griyse direk filtreyi uyguluyor
+            siyahBeyazResim = otsuFiltre.Apply(kaynakResim.PixelFormat != PixelFormat.Format8bppIndexed ? Grayscale.CommonAlgorithms.BT709.Apply(kaynakResim) : kaynakResim);
+
+            BlobCounterBase bc = new BlobCounter();
+            bc.FilterBlobs = true;
+            bc.MinHeight = 5;
+            bc.MinWidth = 5;
+
+            bc.ProcessImage(siyahBeyazResim);
+            blobs = bc.GetObjectsInformation();
+            label2.Text = "Toplam Şekil Sayısı = " + bc.ObjectsCount.ToString();
+            toplamSekilSayisi = bc.ObjectsCount;
+
+            /*aşağıdaki for döngüsü yerine buradaki koşul ifadesi kullanısaydı eğer, 
+            resimdeki en büyük şekil bulunacaktı.
+            if (blobs.Length > 0)
+            {
+                bc.ExtractBlobsImage(siyahBeyazResim, blobs[0], true);
+            }
+            Bitmap xxx = blobs[0].Image.ToManagedImage();
+            */
+
+            //bütün şekiller bulunuyor
+            for (int i = 0, n = blobs.Length; i < n; i++)
+            {
+                if (blobs.Length > 0)
+                {
+
+                    bc.ExtractBlobsImage(siyahBeyazResim, blobs[i], true);
+                    pictureBox1.Image = siyahBeyazResim;
+                    pictureBox1.Refresh();
+                }
+            }
+        }
+
+        //bulunan şekiller arasında sırasıyla dolaşılabilir
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = blobs[sayac - 1].Image.ToManagedImage();
+            label1.Text = "Şekil " + sayac.ToString() + " / " + toplamSekilSayisi.ToString();
+
+            if (sayac == toplamSekilSayisi)
+            {
+                sayac = 0;
+            }
+            sayac++;
+        }
+
+        //kenarlardaki siyah bölgeleri temizler
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //filtre oluşturuldu
+            ExtractBiggestBlob filtre = new ExtractBiggestBlob();
+            //filtre uygulandı
+            Bitmap temizResim = filtre.Apply((Bitmap)pictureBox1.Image);
+            pictureBox1.Image = temizResim;
+        }
+
+        //her şekle farklı bir renk verir
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //filtre oluşturuldu
+            ConnectedComponentsLabeling filtre = new ConnectedComponentsLabeling();
+            Bitmap boyaliResim = filtre.Apply((Bitmap)pictureBox1.Image);
+            pictureBox1.Image = boyaliResim;
+        }
+
+        //verilen sınır değerlerden küçük şekilleri göz ardı eder
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //filtre oluşturuldu
+            BlobsFiltering filtre = new BlobsFiltering();
+            //filtre ayarları yapıldı
+            filtre.CoupledSizeFiltering = true;
+            filtre.MinWidth = 70;
+            filtre.MinHeight = 70;
+
+            //burada bazıları ApplyInPlace() fonksiyonunu kullanıyor
+            //ben Apply() fonksiyonunu kullanıyorum
+            //ApplyInPlace() fonksiyonunu daha önce hiç kullanmadım, nasıl çalıştığınıda çözemedim zaten:)
+            Bitmap newImage = filtre.Apply((Bitmap)pictureBox1.Image); 
+            pictureBox1.Image = newImage;
+        }
+    }
+}
